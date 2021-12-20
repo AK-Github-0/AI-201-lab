f45bde4c25ebadd8b54a0a813d1c03fc9a692381
AK-18 <you@example.com>
2021-12-20 17:36:36 +0500

    Test

---
+++ Users/SELAB/Desktop/TASKS/task.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 20 14:41:00 2021
+
+@author: AK(2020024)
+"""
+
+def encryption(string,shift):
+    Encrypted = ""
+    for i in range(len(string)):
+        char = string[i]
+        Encrypted += chr((ord(char) + shift-65) % 26 + 65)
+ 
+    return Encrypted
+
+print(encryption("DEFENDTHEFORT",7))
